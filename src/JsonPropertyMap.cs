c101415af3fa4d16b6ae19c9643034e4d988785b
Johnny Anderson dos Santos <johnnyadsantos@gmail.com>
2017-10-10 17:57:37 -0300

    Adicionado xmldoc em ingles

--- src/JsonPropertyMap.cs
+++ src/JsonPropertyMap.cs
@@ -5,6 +5,9 @@ using Newtonsoft.Json.Serialization;
 
 namespace JsonFluentMap
 {
+    /// <summary>
+    /// The class for mapping a prop
+    /// </summary>
     public class JsonPropertyMap
     {
         [NotNull] private readonly JsonProperty _jsonProp;
@@ -14,11 +17,19 @@ namespace JsonFluentMap
             _jsonProp = jsonProp;
         }
 
+        /// <summary>
+        /// Ignore this property
+        /// </summary>
         public void Ignore()
         {
             _jsonProp.Ignored = true;
         }
 
+        /// <summary>
+        /// Changes the name of this property
+        /// </summary>
+        /// <param name="name">The name of this property</param>
+        /// <exception cref="ArgumentNullException">Will be thrown when <paramref name="name"/> is null</exception>
         public JsonPropertyMap WithName([NotNull] string name)
         {
             if (name == null)
@@ -27,6 +38,10 @@ namespace JsonFluentMap
             return this;
         }
 
+        /// <summary>
+        /// Changes the converter for this property
+        /// </summary>
+        /// <param name="converter">A json converter</param>
         public JsonPropertyMap ConvertWith(JsonConverter converter)
         {
             _jsonProp.Converter = converter;
