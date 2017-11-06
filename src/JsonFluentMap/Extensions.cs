0a8d55e4f13909cb3fb0b65808204d818e79e72f
Johnny Anderson dos Santos <johnnyasantoss@users.noreply.github.com>
2017-11-06 18:09:27 -0200

    Adiciona nova forma para gerar serializadores (#16)

---
+++ src/JsonFluentMap/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace JsonFluentMap
+{
+    internal static class Extensions
+    {
+        /// <summary>
+        /// Generates a new <see cref="JsonSerializer"/> based on <paramref name="settings"/> or <see cref="BaseSettings"/>
+        /// </summary>
+        /// <param name="contractResolver">The contract resolver to be put in the <see cref="JsonSerializer"/></param>
+        /// <param name="settings">Settings used to create the JsonSerializer</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static JsonSerializer BuildSerializer(
+            [NotNull] this IContractResolver contractResolver,
+            JsonSerializerSettings settings = null
+        )
+        {
+            if (contractResolver == null)
+                throw new ArgumentNullException(nameof(contractResolver));
+
+            if (settings == null)
+                settings = JsonMapSettings.BaseSettings;
+
+            var serializer = new JsonSerializer
+            {
+                CheckAdditionalContent = settings.CheckAdditionalContent,
+                ConstructorHandling = settings.ConstructorHandling,
+                Context = settings.Context,
+                ContractResolver = contractResolver,
+                Culture = settings.Culture,
+                DateFormatHandling = settings.DateFormatHandling,
+                DateFormatString = settings.DateFormatString,
+                DateParseHandling = settings.DateParseHandling,
+                DateTimeZoneHandling = settings.DateTimeZoneHandling,
+                DefaultValueHandling = settings.DefaultValueHandling,
+                EqualityComparer = settings.EqualityComparer,
+                FloatFormatHandling = settings.FloatFormatHandling,
+                FloatParseHandling = settings.FloatParseHandling,
+                Formatting = settings.Formatting,
+                MaxDepth = settings.MaxDepth,
+                MetadataPropertyHandling = settings.MetadataPropertyHandling,
+                MissingMemberHandling = settings.MissingMemberHandling,
+                NullValueHandling = settings.NullValueHandling,
+                ObjectCreationHandling = settings.ObjectCreationHandling,
+                PreserveReferencesHandling = settings.PreserveReferencesHandling,
+                ReferenceLoopHandling = settings.ReferenceLoopHandling,
+                StringEscapeHandling = settings.StringEscapeHandling,
+                TraceWriter = settings.TraceWriter,
+                TypeNameAssemblyFormatHandling = settings.TypeNameAssemblyFormatHandling,
+                TypeNameHandling = settings.TypeNameHandling
+            };
+
+            if (settings.SerializationBinder != null)
+                serializer.SerializationBinder = settings.SerializationBinder;
+
+            var refResolver = settings.ReferenceResolverProvider?.Invoke();
+
+            if (refResolver != null)
+                serializer.ReferenceResolver = refResolver;
+
+            return serializer;
+        }
+    }
+}
---
+++ src/JsonFluentMap/JsonMapSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace JsonFluentMap
+{
+    /// <summary>
+    /// The base settings
+    /// </summary>
+    public static class JsonMapSettings
+    {
+        /// <summary>
+        /// Base settings
+        /// </summary>
+        public static JsonSerializerSettings BaseSettings { get; set; }
+            = new JsonSerializerSettings();
+    }
+}
